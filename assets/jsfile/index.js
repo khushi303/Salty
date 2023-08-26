e8dab451d690f0df0398f64ef6b9d94f22876d05
khushi303 <maankhushi303@gmail.com>
2023-08-26 12:55:52 +0530

    new code

--- assets/jsfile/index.js
+++ assets/jsfile/index.js
@@ -1,4 +1,5 @@
 function navfun() {
     document.getElementById("nav-icon").classList.toggle("start-0");
+    document.body.classList.toggle("overflow-hidden")
     document.getElementById("menu-icon").classList.toggle("cross");
 }
